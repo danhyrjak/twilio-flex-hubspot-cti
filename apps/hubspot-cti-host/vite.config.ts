50b92e412c2baf6d68427a86aab42775d1d46d00
Dan Hyrjak <dan.hyrjak@zing.dev>
2025-08-20 18:15:43 +0000

    updated vite server to allow all hosts, this cleans up the local dev start.
    added .env.example file to the hubspot-cti-host project with keys required before building app.
    
    updated scripts and comments accordingly.

--- apps/hubspot-cti-host/vite.config.ts
+++ apps/hubspot-cti-host/vite.config.ts
@@ -4,6 +4,9 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: true
+  },
   plugins: [
     react(),
      tsconfigPaths({
--- scripts/create-tunnel.sh
+++ scripts/create-tunnel.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo "starting tunnel for use with hubspot-cti-host"
 echo ""
-echo "make sure to copy the tunnel domain into the set-host-env.sh script"
-echo "and run this before running the hubspot-cti-host vite dev server"
+echo "make sure to copy the tunnel URL into the .env file under apps/hubspot-cti-host directory"
+echo "with the key VITE_HUBSPOT_CTI_HOST_ORIGIN before running the hubspot-cti-host vite dev server using:"
+echo "yarn workspace hubspot-cti-host dev"
+echo "or building a production run via:"
+echo "yarn workspace hubspot-cti-host build"
 echo ""
 cloudflared tunnel --url http://localhost:5173

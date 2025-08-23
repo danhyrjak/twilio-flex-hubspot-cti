ab0d36614a01585549d96fd3822580af4c3821b7
Dan Hyrjak <dan.hyrjak@zing.dev>
2025-08-23 15:10:28 +0000

    removed uneeded env variable from hubspot-cti-host and updated instructions. set the port on the hubpost-cti-plugin start command to 3000 to match example config value in host app.

--- apps/hubspot-cti-host/src/schemas/shared.ts
+++ apps/hubspot-cti-host/src/schemas/shared.ts
@@ -4,6 +4,5 @@ export const HubspotCTILocationSchema = z.literal("window").or(z.literal("remote
 export type HubspotCTILocation = z.infer<typeof HubspotCTILocationSchema>;
 
 export const EnvSchema = z.object({
-  VITE_TWILIO_FLEX_ORIGIN: z.string().url(),
-  VITE_HUBSPOT_CTI_HOST_ORIGIN: z.string().url()
+  VITE_TWILIO_FLEX_ORIGIN: z.string().url()
 });
